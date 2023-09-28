357dd50d316cf1be66257dea9a070c1b9dc986bc
Minsuhk <70074654+Minsuhk@users.noreply.github.com>
2023-09-28 20:23:56 +0000

    I have created four lists that will take in the strings of time along with a variable that will take in the minimum meeting duration. I then created a list that will hold the active time of the two participants along with a list that will display when they are available.

---
+++ project1_starter.py
@@ -0,0 +1,11 @@
+import ast
+from functions import combineDailyActive
+
+# Declare variables first before initializing them to each line in input.txt
+var1 = []
+var2 = []
+var3 = []
+var4 = []
+var5 = 0
+dailyActive = []
+available_times = []
