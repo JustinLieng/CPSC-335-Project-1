3b37c832c623e4b64b9b1c196931a19678d59bab
Minsuhk <70074654+Minsuhk@users.noreply.github.com>
2023-09-28 20:21:48 +0000

    I have created a function that will allow us to combine both participants' times where they are active.

--- functions.py
+++ functions.py
@@ -1,3 +1,24 @@
 def convertTimeToMinutes(time):
   hours, minutes = time.split(":")
   return int(hours) * 60 + int(minutes)
+
+# Combines the two people's dailyActive. This gets the greater time of the 0th index of
+# both AND gets the lower time of the 1st index of both.
+def combineDailyActive(person1_dailyActive, person2_dailyActive, dailyActive):
+  p1Lower = convertTimeToMinutes(person1_dailyActive[0])
+  p2Lower = convertTimeToMinutes(person2_dailyActive[0])
+  p1Upper = convertTimeToMinutes(person1_dailyActive[1])
+  p2Upper = convertTimeToMinutes(person2_dailyActive[1])
+  if (p1Lower > p2Lower):
+    dailyActive.append(person1_dailyActive[0])
+  elif (p2Lower > p1Lower):
+    dailyActive.append(person2_dailyActive[0])
+  elif (p1Lower == p2Lower):
+    dailyActive.append(person1_dailyActive[0])
+
+  if (p1Upper < p2Upper):
+    dailyActive.append(person1_dailyActive[1])
+  elif (p2Upper < p1Upper):
+    dailyActive.append(person2_dailyActive[1])
+  elif (p1Upper == p2Upper):
+    dailyActive.append(person1_dailyActive[1])
